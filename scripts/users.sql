cf337abfccab53bdeafdd71e363a6a360ddb826e
Maciej Lewandowski <maciej@encrypted.pl>
2014-07-23 20:49:36 +0200

    Insert root user into database.

--- scripts/users.sql
+++ scripts/users.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `passwd` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Table with users data.' AUTO_INCREMENT=1 ;
+
+-- Insert primary user
+-- login: root
+-- passwd: root (md5)
+INSERT INTO `users` (`id`, `login`, `passwd`) VALUES ('1', 'root', '63a9f0ea7bb98050796b649e85481845');
